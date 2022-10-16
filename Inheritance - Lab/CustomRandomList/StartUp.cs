4ddb22d3d9387747684bf75bc312967a590d588a
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-16 23:37:01 +0300

    Add files via upload

---
+++ Inheritance - Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rrrrr = new RandomList();
+
+            rrrrr.Add("gosho");
+            rrrrr.Add("lqlql");
+            rrrrr.Add("ggggggggg");
+            rrrrr.Add("pkkkkkk");
+
+            var result = rrrrr.RandomString();
+
+            
+        }
+    }
+}
