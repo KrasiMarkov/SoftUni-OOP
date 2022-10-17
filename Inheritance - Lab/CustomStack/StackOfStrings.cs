fd2a75ff4296510679b73e63c13f09813d7e0aa9
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-17 23:37:01 +0300

    Add files via upload

---
+++ Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+            
+        }
+
+        public void AddRange(IEnumerable<string> values)
+        {
+
+            foreach (var item in values)
+            {
+                this.Push(item);
+            }
+            
+        }
+
+
+    }
+}
---
+++ Inheritance - Lab/CustomStack/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var myCoolStack = new StackOfStrings();
+
+            var result = myCoolStack.IsEmpty();
+
+            Console.WriteLine(result);
+
+            myCoolStack.AddRange(new List<string> { "pesho", "gosho" });
+
+            Console.WriteLine(string.Join(" ", myCoolStack));
+        }
+    }
+}
