aabf90ff9c5cfec03cf2344e2b7ef237f9676140
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-23 23:56:22 +0300

    Add files via upload

---
+++ Inheritance - Exercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private double defaultFuelConsumption = 8;
+
+        public RaceMotorcycle(int horsePower, double fuel)
+           : base(horsePower, fuel)
+        {
+
+        }
+
+        public override double FuelConsumption => defaultFuelConsumption;
+
+        public override void Drive(double kilometers)
+        {
+            double fuelAfterDrive = Fuel - kilometers * FuelConsumption;
+
+            if (fuelAfterDrive >= 0)
+            {
+                Fuel = fuelAfterDrive;
+            }
+        }
+    }
+}
---
+++ Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+
+            this.Fuel = fuel;
+            this.HorsePower = horsePower;
+
+        }
+
+      
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            double fuelAfterDrive = Fuel - kilometers * FuelConsumption;
+
+            if (fuelAfterDrive >= 0)
+            {
+                Fuel = fuelAfterDrive;
+            }
+        }
+
+
+    }
+}
