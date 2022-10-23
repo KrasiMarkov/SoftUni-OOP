23361dfb3c32dff234ec25de4b5f59e7fd21e166
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-23 23:56:50 +0300

    Add files via upload

---
+++ Person/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    class Person
+    {
+
+        private string name;
+
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+
+
+                this.age = value;
+
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {this.Name}, Age: {this.Age}");
+
+            return sb.ToString();
+
+        }
+    }
+}
