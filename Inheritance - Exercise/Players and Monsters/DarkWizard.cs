853b418fc71c1c83811eb3136d8b2bfa48265871
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-25 23:07:54 +0300

    Add files via upload

---
+++ Inheritance - Exercise/Players and Monsters/DarkWizard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class DarkWizard : Wizard
+    {
+        public DarkWizard(string username, int level)
+            : base(username, level)
+        {
+
+
+
+        }
+
+    }
+}
---
+++ Inheritance - Exercise/Players and Monsters/Wizard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string username, int level)
+            : base(username, level)
+        {
+
+        }
+
+    }
+}
