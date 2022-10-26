5d15454aa89d9d1b86f99a2554fd40d449d62696
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-26 23:17:30 +0300

    Add files via upload

---
+++ Inheritance - Exercise/Zoo/Snake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Snake : Reptile
+    {
+        public Snake(string name)
+            : base (name)
+        {
+
+        }
+
+    }
+}
