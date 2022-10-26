413a3e661f51170a952e72f2e9aea244bf890588
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-26 23:16:42 +0300

    Add files via upload

---
+++ Inheritance - Exercise/Restaurant/HotBeverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class HotBeverage : Beverage
+    {
+        public HotBeverage(string name, decimal price, double milliliters)
+            : base(name, price, milliliters)
+        {
+
+        }
+
+    }
+}
