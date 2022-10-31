476eff4f3d739fbe3e5510a91b2c2f2931522d88
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-31 23:28:32 +0200

    Add files via upload

---
+++ Encapsulation - Exercise/AnimalFarm/AnimalFarm/AnimalFarm/Chicken.cs
@@ -0,0 +1,95 @@
+﻿namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new System.Exception("Name cannot be empty.");
+                }
+
+                else
+                {
+                    this.name = value;
+                }
+
+
+
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (value >= MinAge && value <= MaxAge)
+                {
+                    this.age = value;
+                }
+
+                else
+                {
+                    throw new System.Exception("Age should be between 0 and 15.");
+                }
+                
+            }
+        }
+
+        public double ProductPerDay
+        {
+			get
+			{				
+				return this.CalculateProductPerDay();
+			}
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.Age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
