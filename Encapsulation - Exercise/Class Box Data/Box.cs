dc1c8a1442c376ecfab3ce56400acdecdedf037b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-10-31 23:29:47 +0200

    Add files via upload

---
+++ Encapsulation - Exercise/Class Box Data/Box.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Box_Data
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+
+            private set
+            {
+                if (value > 0)
+                {
+                    this.length = value;
+                }
+
+                else
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+            }
+
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            private set
+            {
+                if (value > 0)
+                {
+                    this.width = value;
+                }
+
+                else
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+            }
+
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            private set
+            {
+                if (value > 0)
+                {
+                    this.height = value;
+                }
+
+                else
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+            }
+
+        }
+
+        public double CalculateSurfaceArea()
+        {
+
+            double surfaceArea = (2 * length * width) + (2 * length * height) + (2 * width * height);
+
+            return surfaceArea;
+
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+
+            double lateralSurfaceArea = (2 * length * height) + (2 * width * height);
+
+            return lateralSurfaceArea;
+        }
+
+        public double CalculateVolume()
+        {
+
+            double volume = length * width * height;
+
+            return volume;
+        }
+
+    }
+}
---
+++ Encapsulation - Exercise/Class Box Data/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Class_Box_Data
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+
+            double width = double.Parse(Console.ReadLine());
+
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                double surfaceArea = box.CalculateSurfaceArea();
+
+                double lateralSurfaceArea = box.CalculateLateralSurfaceArea();
+
+                double volume = box.CalculateVolume();
+
+                Console.WriteLine($"Surface Area - {surfaceArea:F2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:F2}");
+                Console.WriteLine($"Volume - {volume:F2}");
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
