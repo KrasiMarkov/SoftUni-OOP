9f240f6cc75fc5d6262999e6fb703c470e5c03f0
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-02 23:17:03 +0200

    Add files via upload

---
+++ Lab_Interfaces and Abstraction/Cars/Shapes/IDrawable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public interface IDrawable
+    {
+
+        public void Draw();
+        
+            
+        
+    }
+}
