2e96c9e9cbf196364b827bd88d9bb1952faa7f91
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-03 23:34:24 +0200

    Add files via upload

---
+++ Exercise_Interfaces and Abstraction/Define an Interface IPerson/IIdentifiable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public interface IIdentifiable
+    {
+        string Id { get; }
+
+
+
+    }
+}
