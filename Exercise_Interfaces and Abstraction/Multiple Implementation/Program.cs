4ffc6989019e0746d702b02eb7c82727f0685f7b
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-04 23:41:12 +0200

    Add files via upload

---
+++ Exercise_Interfaces and Abstraction/Multiple Implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiple_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
