2aa9e203c2f446823c8b91e3dabc6c53d75ff905
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-07 23:31:02 +0200

    Add files via upload

---
+++ Lab_Polymorphism/Operations/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+
+            return c;
+        }
+
+        public double Add(double a, double b, double c)
+        {
+            double d = a + b + c;
+
+            return d;
+        }
+
+        public decimal Add(decimal a, decimal b, decimal c)
+        {
+            decimal d = a + b + c;
+
+            return d;
+        }
+
+    }
+}
---
+++ Lab_Polymorphism/Operations/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operations
+{
+    public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine(math.Add(4.5, 5.5, 7.6));
+
+            Console.WriteLine(math.Add(6, 7));
+
+            Console.WriteLine(math.Add(4.5m, 2.4m, 7.7m));
+        }
+    }
+}
