c0626ee8ee0fc9fbc4f1cd462d2ef6157501fc9e
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-07 23:30:16 +0200

    Add files via upload

---
+++ Lab_Polymorphism/Animals/Animal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string FavouriteFood
+        {
+            get
+            {
+                return this.favouriteFood;
+            }
+
+            set
+            {
+                this.favouriteFood = value;
+            }
+        }
+
+
+
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+
+        }
+        
+
+
+           
+        
+
+    }
+}
