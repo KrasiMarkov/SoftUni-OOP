1743abbf3f4985e872d8456de1f0d7fd1bd6e8ce
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-08 23:52:36 +0200

    Add files via upload

---
+++ Lab_Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+            private set
+            {
+                this.radius = value;
+            }
+
+        }
+
+        public override double CalculateArea()
+        {
+            double area = Math.PI * radius * radius;
+
+            return area;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            double Perimeter = 2 * Math.PI * radius;
+
+            return Perimeter;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+
+    }
+}
---
+++ Lab_Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set 
+            {
+                this.height = value;
+            }
+
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                this.width = value;
+            }
+
+        }
+
+        public override double CalculateArea()
+        {
+            double area = this.height * this.width;
+
+            return area;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            double perimeter = 2 * (this.height + this.width);
+
+            return perimeter;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+
+        }
+
+    }
+}
