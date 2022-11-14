1be8adb9825a880247622babcc986190208101d7
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-14 23:26:44 +0200

    Add files via upload

---
+++ Exercise_Exception Handling/Square Root/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                double result = Math.Sqrt(n);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+            //finally
+            //{
+            //    Console.WriteLine("Krasi");
+            //}
+
+        }
+    }
+}
