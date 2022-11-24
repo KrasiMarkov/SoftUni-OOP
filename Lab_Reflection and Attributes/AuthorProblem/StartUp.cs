01f7ffa101e2f817b873f1b468322e9941e6b1e6
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-24 23:03:06 +0200

    Add files via upload

---
+++ Lab_Reflection and Attributes/AuthorProblem/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuthorProblem
+{[Author("Krasi")]
+    public class StartUp
+    {
+        [Author("Milko")]
+        static void Main(string[] args)
+        {
+            
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+
+        }
+    }
+}
