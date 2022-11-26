cdcc1ee5e9a87f98f1477b7c6830f2f79feeb695
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-11-26 23:46:54 +0200

    Add files via upload

---
+++ ReflectionAndAttributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,58 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+using CommandPattern.Core.Commands;
+
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string commandTikTok = "Command";
+        public string Read(string args)
+        {
+            string[] tokens = args.Split().ToArray();
+
+            string commandName = tokens[0];
+
+            string commandTypeName = commandName + commandTikTok;
+
+            Type commandType = Assembly.GetCallingAssembly()
+            .GetTypes()
+            .Where(t => t.GetInterfaces().Any(i => i.Name == nameof(ICommand)))
+            .FirstOrDefault(t => t.Name == commandTypeName);
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Command type is invalid!");
+            }
+
+
+
+
+            ICommand command = Activator.CreateInstance(commandType) as ICommand;
+
+            //if (commandName == "Hello")
+            //{
+               
+            //    command = new HelloCommand();
+
+            //}
+
+            //else if (commandName == "Exit")
+            //{
+            //    command = new ExitCommand();
+
+            //}
+
+            string[] cleared = tokens.Skip(1).ToArray();
+
+            string result = command.Execute(cleared);
+
+            return result;
+        }
+    }
+}
---
+++ ReflectionAndAttributes/CommandPattern/Core/Engine.cs
@@ -0,0 +1,32 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string inputData = Console.ReadLine();
+
+                string result = this.commandInterpreter.Read(inputData);
+
+                Console.WriteLine(result);
+
+            }
+
+        }
+    }
+}
