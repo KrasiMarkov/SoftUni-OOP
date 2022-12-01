ad02bc501f93e1a494a4bec527286e456f53b947
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-01 23:36:37 +0200

    Add files via upload

---
+++ ReflectionAndAttributes/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+
+        private int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            int intObj = Convert.ToInt32(obj);
+
+            if (intObj >= this.minValue && intObj <= maxValue)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
---
+++ ReflectionAndAttributes/ValidationAttributes/Validator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+
+
+namespace ValidationAttributes
+{
+    public static class Validator 
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objType = obj.GetType();
+
+            PropertyInfo[] propertyInfos = objType.GetProperties();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+
+                List<MyValidationAttribute> allMyValidationAttributes = propertyInfo.GetCustomAttributes<MyValidationAttribute>().ToList();
+
+                object propertyObj = propertyInfo.GetValue(obj);
+
+                foreach (MyValidationAttribute myValidationAttribute in allMyValidationAttributes)
+                {
+
+                    bool isValid = myValidationAttribute.IsValid(propertyObj);
+
+                    if (!isValid)
+                    {
+                        return false;
+                    }
+
+
+                }
+
+            }
+
+            return true;
+        }
+
+    }
+}
