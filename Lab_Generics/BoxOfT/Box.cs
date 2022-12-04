a73413ef5389c1ccb129419e38c9be0e778c1e33
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-04 23:10:56 +0200

    Add files via upload

---
+++ Lab_Generics/BoxOfT/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> data;
+
+        public Box()
+        {
+            this.data = new Stack<T>();
+
+        }
+
+        public int Count
+        {
+           get
+           { 
+               return this.data.Count;
+
+           }
+
+        }
+        public void Add(T item)
+        {
+            data.Push(item);
+        }
+
+        public T Remove()
+        {
+             return data.Pop();
+        }
+
+    }
+}
---
+++ Lab_Generics/BoxOfT/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> myStack = new Box<int>();
+
+            myStack.Add(10);
+            myStack.Add(40);
+
+            int result = myStack.Count;
+
+            myStack.Remove();
+            int finalResult = myStack.Count;
+
+
+            Console.WriteLine(result);
+            Console.WriteLine(finalResult);
+
+            Console.WriteLine(myStack.Remove());
+
+        }
+    }
+}
