b273090a074de22e63d9f06b384aed93381a819c
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-06 23:48:19 +0200

    Add files via upload

---
+++ Lab_Generics/GenericScale/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var myScale = new EqualityScale<int>(5, 5);
+            var result = myScale.AreEqual();
+
+            Console.WriteLine(result);
+        }
+    }
+}
