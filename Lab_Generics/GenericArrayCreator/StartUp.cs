fd069cb15e9d4e5c7ec121f2faefe55eef560a65
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-06 23:47:46 +0200

    Add files via upload

---
+++ Lab_Generics/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] myArr = ArrayCreator.Create(5, "Pesho");
+
+            int[] intove = ArrayCreator.Create(6, 7);
+
+            Console.WriteLine(string.Join(" ", myArr));
+
+            Console.WriteLine(string.Join(" ", intove));
+        }
+    }
+}
