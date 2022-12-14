99ab8fda9e94980e56952fa735bca47bce706785
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-14 23:31:05 +0200

    Add files via upload

---
+++ Exercise_Generics/Generic Count Method Doubles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Count_Method_Doubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
