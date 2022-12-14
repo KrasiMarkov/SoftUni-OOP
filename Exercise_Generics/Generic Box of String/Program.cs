f0503647870b4e822dcca2eda7bd2de8a5de1ea5
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-14 23:30:34 +0200

    Add files via upload

---
+++ Exercise_Generics/Generic Box of String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Box_of_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
