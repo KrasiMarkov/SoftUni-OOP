ed5449340d21e242c2ed04d5433948be18fd4946
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-16 23:40:18 +0200

    Add files via upload

---
+++ Exercise_Generics/Generic Swap Method Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Swap_Method_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
