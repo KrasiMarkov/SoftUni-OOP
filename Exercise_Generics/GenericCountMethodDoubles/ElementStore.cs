5271bf5fadf766d6023bbc89cd8f2db9b8bb6a74
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-18 23:04:54 +0200

    Add files via upload

---
+++ Exercise_Generics/GenericCountMethodDoubles/ElementStore.cs
@@ -0,0 +1,44 @@
+﻿using GenericBoxOfString;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodDoubles
+{
+    public class ElementStore<T> where T : IComparable
+    {
+        private List<Box<T>> boxes;
+
+        public ElementStore()
+        {
+            this.boxes = new List<Box<T>>();
+
+        }
+
+        public void AddToBoxes(Box<T> box)
+        {
+            boxes.Add(box);
+        }
+
+        public int CompareElements(T element)
+        {
+            Box<T> currentBox = new Box<T>(element);
+
+            int counter = 0;
+
+            foreach (Box<T> box in this.boxes)
+            {
+                if (currentBox.IsLower(box))
+                {
+                    counter++;
+                }
+
+
+            }
+
+            return counter;
+        }
+
+
+    }
+}
---
+++ Exercise_Generics/GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using GenericBoxOfString;
+using System;
+
+namespace GenericCountMethodDoubles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            ElementStore<double> elementStore = new ElementStore<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double text = double.Parse(Console.ReadLine());
+
+                Box<double> mybox = new Box<double>(text);
+
+                elementStore.AddToBoxes(mybox);
+
+            }
+
+            double element = double.Parse(Console.ReadLine());
+
+            int result = elementStore.CompareElements(element);
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
