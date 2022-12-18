7f9686a67a164b14dd43a8e25db6aa147e6ab0db
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-18 23:04:17 +0200

    Add files via upload

---
+++ Exercise_Generics/GenericBoxОfInteger/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxОfInteger
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"{item.GetType()}: {this.item}";
+        }
+    }
+}
---
+++ Exercise_Generics/GenericBoxОfInteger/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericBoxОfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                Box<int> myBox = new Box<int>(number);
+
+                Console.WriteLine(myBox);
+            }
+
+
+
+        }
+    }
+}
