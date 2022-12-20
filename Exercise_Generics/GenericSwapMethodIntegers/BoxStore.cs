9180c49d1d75901d734a80cd6ef57692ace23dbc
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-20 23:14:00 +0200

    Add files via upload

---
+++ Exercise_Generics/GenericSwapMethodIntegers/BoxStore.cs
@@ -0,0 +1,47 @@
+﻿using GenericBoxОfInteger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+    public class BoxStore<T>
+    {
+        List<Box<T>> totalBoxes;
+
+        public BoxStore()
+        {
+            this.totalBoxes = new List<Box<T>>();
+        }
+
+        public void AddToTotalBoxes(Box<T> box)
+        {
+            totalBoxes.Add(box);
+        }
+
+        public void SwapBoxes(int firstIndex, int secondIndex)
+        {
+            var current = totalBoxes[firstIndex];
+
+            var secondCurrent = totalBoxes[secondIndex];
+
+            totalBoxes[firstIndex] = secondCurrent;
+
+            totalBoxes[secondIndex] = current;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Box<T> box in totalBoxes)
+            {
+                sb.AppendLine(box.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+
+    }
+}
---
+++ Exercise_Generics/GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using GenericBoxОfInteger;
+
+namespace GenericSwapMethodIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BoxStore<int> finalBoxes = new BoxStore<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                Box<int> myBox = new Box<int>(number);
+
+                finalBoxes.AddToTotalBoxes(myBox);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int firstIndex = indexes[0];
+
+            int secondIndex = indexes[1];
+
+            finalBoxes.SwapBoxes(firstIndex, secondIndex);
+
+            Console.WriteLine(finalBoxes);
+        }
+    }
+    
+}
