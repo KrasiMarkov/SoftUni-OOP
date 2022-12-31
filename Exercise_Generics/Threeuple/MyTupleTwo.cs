5cc47cee5a5c040b33250ad44c3bd4a6b6a8ccba
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2022-12-31 13:14:15 +0200

    Add files via upload

---
+++ Exercise_Generics/Threeuple/MyTupleTwo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class MyTupleTwo<TItem1, TItem2, TItem3>
+    {
+
+        private TItem1 Item1;
+
+        private TItem2 Item2;
+
+        private TItem3 Item3;
+
+        public MyTupleTwo(TItem1 Item1, TItem2 Item2, TItem3 Item3)
+        {
+            this.Item1 = Item1;
+            this.Item2 = Item2;
+            this.Item3 = Item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2} -> {this.Item3}";
+        }
+    }
+}
---
+++ Exercise_Generics/Threeuple/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split().ToArray();
+
+            string fullName = $"{text[0]} {text[1]}";
+
+            string secondElement = text[2];
+
+            string town = $"{text[3]} {text[4]}";
+
+            MyTupleTwo<string, string, string> myTuple = new MyTupleTwo<string, string, string>(fullName, text[2], town);
+            Console.WriteLine(myTuple);
+
+            string[] myArr = Console.ReadLine().Split().ToArray();
+
+            MyTupleTwo<string, int, string> myTuple1 = new MyTupleTwo<string, int, string>(myArr[0], int.Parse(myArr[1]), myArr[2]);
+            Console.WriteLine(myTuple1);
+
+
+            string[] myArrFinal = Console.ReadLine().Split().ToArray();
+
+            MyTupleTwo<string, double, string> myTuple2 = new MyTupleTwo<string, double, string>(myArrFinal[0], double.Parse(myArrFinal[1]), myArrFinal[2]);
+
+            Console.WriteLine(myTuple2);
+        }
+    }
+    
+}
