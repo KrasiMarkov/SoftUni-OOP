f3f15ca8e9d91f92ffca91fcba6b055858a89190
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-01-07 23:18:40 +0200

    Add files via upload

---
+++ Exercise_Generics/Tuple/MyTuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<TItem1, TItem2>
+    {
+        private TItem1 Item1;
+
+        private TItem2 Item2;
+
+        public MyTuple(TItem1 Item1, TItem2 Item2)
+        {
+            this.Item1 = Item1;
+            this.Item2 = Item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
---
+++ Exercise_Generics/Tuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Tuple
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Tuple<string, int, string> myFirstTuple = new Tuple<string, int, string>("krasi", 28, "markov");
+
+            //Console.WriteLine(myFirstTuple.Item3);
+
+            string[] text = Console.ReadLine().Split().ToArray();
+
+            string fullName = $"{text[0]} {text[1]}";
+
+            MyTuple<string, string> myTuple = new MyTuple<string, string>(fullName, text[2]);
+            Console.WriteLine(myTuple);
+
+            string[] myArr = Console.ReadLine().Split().ToArray();
+
+            MyTuple<string, int> myTuple1 = new MyTuple<string, int>(myArr[0], int.Parse(myArr[1]));
+            Console.WriteLine(myTuple1);
+
+
+            string[] myArrFinal = Console.ReadLine().Split().ToArray();
+
+            MyTuple<int, double> myTuple2 = new MyTuple<int, double>(int.Parse(myArrFinal[0]), double.Parse(myArrFinal[1]));
+
+            Console.WriteLine(myTuple2);
+        }
+    }
+}
