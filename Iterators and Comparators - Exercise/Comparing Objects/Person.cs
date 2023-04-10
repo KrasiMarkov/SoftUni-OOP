22f62861a766c96bc799b70190fd07e461e89ec1
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-04-10 23:26:42 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/Comparing Objects/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+
+        }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            var nameComparison = this.Name.CompareTo(other.Name);
+
+            if (nameComparison == 0)
+            {
+                var ageComparison = this.Age.CompareTo(other.Age);
+
+                if (ageComparison == 0)
+                {
+                    var townComparison = this.Town.CompareTo(other.Town);
+
+                    if (townComparison == 0)
+                    {
+                        return 0;
+                    }
+
+                    return townComparison;
+                }
+
+                return ageComparison;
+            }
+
+            return nameComparison;
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/Comparing Objects/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> people = new List<Person>();
+
+            while (true)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+
+                if (data[0] == "END")
+                {
+                    break;
+                }
+
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                string town = data[2];
+
+                Person person = new Person(name, age, town);
+
+
+                people.Add(person);
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < people.Count)
+            {
+                int countOfMathes = 0;
+
+                int numberOfNotEqualPeople = 0;
+
+                int totalNumberOfPeople = 0;
+
+                Person person1 = people[n];
+
+                foreach (var item in people)
+                {
+                    totalNumberOfPeople++;
+
+                    var result = item.CompareTo(person1);
+
+                    if (result == 0)
+                    {
+                        countOfMathes++;
+                    }
+
+                    else
+                    {
+                        numberOfNotEqualPeople++;
+                    }
+                }
+
+                Console.WriteLine($"{countOfMathes} {numberOfNotEqualPeople} {totalNumberOfPeople}");
+            }
+
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
