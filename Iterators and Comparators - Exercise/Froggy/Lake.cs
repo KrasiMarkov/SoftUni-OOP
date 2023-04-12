a3d009eb0a82e92cf81be236429f7f9532c4c84f
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-04-12 23:56:11 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> MyList;
+
+        public Lake(int[] numbers)
+        {
+            this.MyList = numbers.ToList();
+        }
+
+       
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.MyList.Count; i+=2)
+            {
+                yield return this.MyList[i];
+
+            }
+
+            int lastOddIndex = this.MyList.Count - 1;
+
+            if (lastOddIndex % 2 == 0)
+            {
+                lastOddIndex--;
+            }
+
+            for (int i = lastOddIndex; i >= 0; i-=2)
+            {
+                yield return this.MyList[i];
+            }
+
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/Froggy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Lake stones = new Lake(numbers);
+
+            Console.WriteLine(string.Join(", ", stones));
+
+        }
+    }
+}
