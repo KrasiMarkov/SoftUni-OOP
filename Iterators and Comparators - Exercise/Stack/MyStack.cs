3e17892e31a8a7e6a66dc64597694e23f500c2da
Krasimir Markov <64689391+KrasiMarkov@users.noreply.github.com>
2023-04-19 23:06:07 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/Stack/MyStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private Stack<T> MyCustomStack;
+
+        public MyStack()
+        {
+            this.MyCustomStack = new Stack<T>();
+
+
+        }
+        public void Push(T current)
+        {
+            MyCustomStack.Push(current);
+        }
+
+        public void Pop()
+        {
+            if (MyCustomStack.Count > 0)
+            {
+                MyCustomStack.Pop();
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+            
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in MyCustomStack)
+            {
+                yield return item;
+
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/Stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<string> Stackche = new MyStack<string>();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (commands[0] == "END")
+                {
+                    foreach (var item in Stackche)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    foreach (var item in Stackche)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                }
+
+                else if (commands[0] == "Push")
+                {
+                    List<string> myList = new List<string>(commands.Skip(1));
+
+                    for (int i = 0; i < myList.Count; i++)
+                    {
+                        string current = myList[i];
+
+                        Stackche.Push(current);
+                    }
+                }
+
+                else if (commands[0] == "Pop")
+                {
+                    Stackche.Pop();
+                }
+            }
+        }
+    }
+}
